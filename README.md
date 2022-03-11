fc46171f7c81da08a83f40f1757e48e643f03e2c
Franccesco Orozco <franccesco@codingdose.info>
2022-03-10 22:45:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# DN-Reporter
+
+This is a small package that is used to identify errors in Deepnote / Jupyter notebooks without having to wrap your code in a `try/else` statement.
+
+The error can happen in any cell, and it will notify the data science team via Slack (or any HTTP endpoint).
+
+
+## Installation
+
+To install it you will need your user token and then install the package from the repository directly.
+
+```
+pip install https://github.com/franccesco/dn-reporter@main
+```
+
+## Usage/Examples
+
+Once the package is installed, you can create a new Deepnote project and then initialize the `DNReporter` class and pass an endpoint URL and your Deepnote project name:
+
+```python
+from dnreporter import DNReporter
+
+reporter = DNReporter('https://endpoint.com', 'report_name')
+```
+
+The initialization process will automatically override the usual halt process when an exception is encountered in any cell. If you want to disable the automatic override you can pass the `auto_override=False` argument, and then initialize it yourself:
+
+```python
+reporter = DNReporter('endpoint_url', 'report_name', auto_override=False)
+reporter.override_failure()
+```
+
+At the end of the notebook, you can use the method `.report_sucess()` to send a message to Slack reporting that the project was successfully executed:
+
+```python
+reporter.report_success()
+```
---
+++ dnreporter/dnreporter.py
@@ -0,0 +1,43 @@
+import time
+import requests
+from IPython.core.ultratb import AutoFormattedTB
+
+
+class DNReporter:
+    """Report Deepnote's project status to Slack"""
+
+    def __init__(self, endpoint_url, report_name, project_url=None, auto_override=True):
+        self.endpoint_url = endpoint_url
+        self.report_name = report_name
+        self.project_url = project_url
+        self.start_time = time.time()
+        itb = AutoFormattedTB(mode="Plain", tb_offset=1)
+        if auto_override:
+            self.override_failure()
+
+    def execution_time(self):
+        execution_time = round(time.time() - self.start_time, 2)
+        return execution_time
+
+    def report_status(self, status, execution_time=None):
+        if execution_time is None:
+            execution_time = self.execution_time()
+        req_data = {
+            "name": self.report_name,
+            "url": self.project_url,
+            "execution_time": str(execution_time),
+            "status": status,
+        }
+        response = requests.post(self.endpoint_url, json=req_data)
+        print(f"📣 Project {status}; report was sent to Slack.")
+
+    def custom_exc(self, shell, etype, evalue, tb, tb_offset=None):
+        execution_time = self.execution_time()
+        shell.showtraceback((etype, evalue, tb), tb_offset=tb_offset)
+        self.report_status("Failed")
+
+    def report_success(self):
+        self.report_status("Successful")
+
+    def override_failure(self):
+        return get_ipython().set_custom_exc((Exception,), self.custom_exc)
